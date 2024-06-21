f3e71d464c29a3cddff3e4ecdaa491f5fcf23a81
samarsasssi <samar.sassi@esprit.tn>
2024-06-21 02:36:38 +0200

    activate

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -5,6 +5,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import { ActivateComponent } from './activate/activate.component';
 
 const routes: Routes = [{
   path: '',
@@ -13,6 +14,10 @@ const routes: Routes = [{
     {
       path: 'dashboard',
       component: ECommerceComponent,
+    },
+    {
+    path: 'activate',
+    component: ActivateComponent,
     },
     {
       path: 'iot-dashboard',
